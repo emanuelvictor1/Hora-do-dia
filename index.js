c3cdc2effc264302a733db450ab9763a610b40cf
Victor Emanuel <119902446+emanuelvictor1@users.noreply.github.com>
2023-01-24 23:11:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ function verificar() {
     
     if (h >= 0 && h < 12) {
         titulo.innerHTML = `Agora são ${h} horas da manhã.`
-        img.src = "https://github.com/emanuelvictor1/Hora-do-dia/blob/master/img/manha.jpg";
+        img.src = "/img/manha.jpg";
         document.body.style.background = '#F5DEB3'
     }
     if (h >= 12 && h < 18) {
         titulo.innerHTML = `Agora são ${h} horas da tarde.`
-        img.src = "https://github.com/emanuelvictor1/Hora-do-dia/blob/master/img/foto%20tarde.jpg";
+        img.src = "/img/foto tarde.jpg";
         document.body.style.background = '#F4A460'
     }
     if (h >= 18) {
         titulo.innerHTML = `Agora são ${h} horas da noite.`
-        img.src = "https://github.com/emanuelvictor1/Hora-do-dia/blob/master/img/foto%20noite.jpg";
+        img.src = "/img/foto noite.jpg";
         document.body.style.background = '	#8B4513'
     }
     if (h >= 00 && h < 5) {
         titulo.innerHTML = `Agora são ${h} horas da madrugada.`
-        img.src = "https://github.com/emanuelvictor1/Hora-do-dia/blob/master/img/foto%20madrugada.jpg";
+        img.src = "/img/foto madrugada.jpg";
         document.body.style.background = '#363636'
         
     }
