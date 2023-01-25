d90eba07060993a0a255894fccd61c6fc1c02ce4
emanuelvictor <nunesvictoremanuel3@gmail.com>
2023-01-25 01:39:54 -0300

    Hora do dia

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function verificar() {
+    var titulo = document.getElementById('msg')
+    var img = document.getElementById("img")
+
+    var hora = new Date()
+    var h = hora.getHours() 
+
+    
+    if (h >= 0 && h < 12) {
+        titulo.innerHTML = `Agora são ${h} horas da manhã.`
+        img.src = "/img/manha.jpg";
+        document.body.style.background = '#F5DEB3'
+    }
+    if (h >= 12 && h < 18) {
+        titulo.innerHTML = `Agora são ${h} horas da tarde.`
+        img.src = "/img/foto tarde.jpg";
+        document.body.style.background = '#F4A460'
+    }
+    if (h >= 18) {
+        titulo.innerHTML = `Agora são ${h} horas da noite.`
+        img.src = "/img/foto noite.jpg";
+        document.body.style.background = '	#8B4513'
+    }
+    if (h >= 00 && h < 5) {
+        titulo.innerHTML = `Agora são ${h} horas da madrugada.`
+        img.src = "/img/foto madrugada.jpg";
+        document.body.style.background = '#363636'
+        
+    }
+
+}
+
+
+
